2d7f9fb179ec721d0ae55ce9a1b4ba7b6c11de3d
pjpure <pure254326@gmail.com>
2022-11-05 05:13:11 +0700

    add pygame

--- sokoban.py
+++ sokoban.py
@@ -3,10 +3,15 @@ import collections
 import numpy as np
 import heapq
 import time
+from render import Renderer
+from board import BoardManager
+import pygame
+
 
 class PriorityQueue:
     """Define a PriorityQueue data structure that will be used"""
-    def  __init__(self):
+
+    def __init__(self):
         self.Heap = []
         self.Count = 0
 
@@ -22,136 +27,167 @@ class PriorityQueue:
     def isEmpty(self):
         return len(self.Heap) == 0
 
+
 """Load puzzles and define the rules of sokoban"""
+
 
 def transferToGameState(layout):
     """Transfer the layout of initial puzzle"""
-    layout = [x.replace('\n','') for x in layout]
+    layout = [x.replace('\n', '') for x in layout]
     layout = [','.join(layout[i]) for i in range(len(layout))]
     layout = [x.split(',') for x in layout]
     maxColsNum = max([len(x) for x in layout])
     for irow in range(len(layout)):
         for icol in range(len(layout[irow])):
-            if layout[irow][icol] == ' ': layout[irow][icol] = 0   # free space
-            elif layout[irow][icol] == '#': layout[irow][icol] = 1 # wall
-            elif layout[irow][icol] == '&': layout[irow][icol] = 2 # player
-            elif layout[irow][icol] == 'B': layout[irow][icol] = 3 # box
-            elif layout[irow][icol] == '.': layout[irow][icol] = 4 # goal
-            elif layout[irow][icol] == 'X': layout[irow][icol] = 5 # box on goal
+            if layout[irow][icol] == ' ':
+                layout[irow][icol] = 0   # free space
+            elif layout[irow][icol] == '#':
+                layout[irow][icol] = 1  # wall
+            elif layout[irow][icol] == '&':
+                layout[irow][icol] = 2  # player
+            elif layout[irow][icol] == 'B':
+                layout[irow][icol] = 3  # box
+            elif layout[irow][icol] == '.':
+                layout[irow][icol] = 4  # goal
+            elif layout[irow][icol] == 'X':
+                layout[irow][icol] = 5  # box on goal
         colsNum = len(layout[irow])
         if colsNum < maxColsNum:
-            layout[irow].extend([1 for _ in range(maxColsNum-colsNum)]) 
+            layout[irow].extend([1 for _ in range(maxColsNum-colsNum)])
     return np.array(layout)
+
 
 def PosOfPlayer(gameState):
     """Return the position of agent"""
-    return tuple(np.argwhere(gameState == 2)[0]) # e.g. (2, 2)
+    return tuple(np.argwhere(gameState == 2)[0])  # e.g. (2, 2)
+
 
 def PosOfBoxes(gameState):
     """Return the positions of boxes"""
-    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5))) # e.g. ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5)))  # e.g. ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+
 
 def PosOfWalls(gameState):
     """Return the positions of walls"""
-    return tuple(tuple(x) for x in np.argwhere(gameState == 1)) # e.g. like those above
+    return tuple(tuple(x) for x in np.argwhere(gameState == 1))  # e.g. like those above
+
 
 def PosOfGoals(gameState):
     """Return the positions of goals"""
-    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5))) # e.g. like those above
+    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5)))  # e.g. like those above
+
 
 def isEndState(posBox):
     """Check if all boxes are on the goals (i.e. pass the game)"""
     return sorted(posBox) == sorted(posGoals)
 
+
 def isLegalAction(action, posPlayer, posBox):
     """Check if the given action is legal"""
     xPlayer, yPlayer = posPlayer
-    if action[-1].isupper(): # the move was a push
+    if action[-1].isupper():  # the move was a push
         x1, y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
     else:
         x1, y1 = xPlayer + action[0], yPlayer + action[1]
     return (x1, y1) not in posBox + posWalls
 
+
 def legalActions(posPlayer, posBox):
     """Return all legal actions for the agent in the current game state"""
-    allActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    allActions = [[-1, 0, 'u', 'U'], [1, 0, 'd', 'D'],
+                  [0, -1, 'l', 'L'], [0, 1, 'r', 'R']]
     xPlayer, yPlayer = posPlayer
     legalActions = []
     for action in allActions:
         x1, y1 = xPlayer + action[0], yPlayer + action[1]
-        if (x1, y1) in posBox: # the move was a push
-            action.pop(2) # drop the little letter
+        if (x1, y1) in posBox:  # the move was a push
+            action.pop(2)  # drop the little letter
         else:
-            action.pop(3) # drop the upper letter
+            action.pop(3)  # drop the upper letter
         if isLegalAction(action, posPlayer, posBox):
             legalActions.append(action)
-        else: 
-            continue     
-    return tuple(tuple(x) for x in legalActions) # e.g. ((0, -1, 'l'), (0, 1, 'R'))
+        else:
+            continue
+    # e.g. ((0, -1, 'l'), (0, 1, 'R'))
+    return tuple(tuple(x) for x in legalActions)
+
 
 def updateState(posPlayer, posBox, action):
     """Return updated game state after an action is taken"""
-    xPlayer, yPlayer = posPlayer # the previous position of player
-    newPosPlayer = [xPlayer + action[0], yPlayer + action[1]] # the current position of player
+    xPlayer, yPlayer = posPlayer  # the previous position of player
+    newPosPlayer = [xPlayer + action[0], yPlayer +
+                    action[1]]  # the current position of player
     posBox = [list(x) for x in posBox]
-    if action[-1].isupper(): # if pushing, update the position of box
+    if action[-1].isupper():  # if pushing, update the position of box
         posBox.remove(newPosPlayer)
         posBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
     posBox = tuple(tuple(x) for x in posBox)
     newPosPlayer = tuple(newPosPlayer)
     return newPosPlayer, posBox
 
+
 def isFailed(posBox):
     """This function used to observe if the state is potentially failed, then prune the search"""
-    rotatePattern = [[0,1,2,3,4,5,6,7,8],
-                    [2,5,8,1,4,7,0,3,6],
-                    [0,1,2,3,4,5,6,7,8][::-1],
-                    [2,5,8,1,4,7,0,3,6][::-1]]
-    flipPattern = [[2,1,0,5,4,3,8,7,6],
-                    [0,3,6,1,4,7,2,5,8],
-                    [2,1,0,5,4,3,8,7,6][::-1],
-                    [0,3,6,1,4,7,2,5,8][::-1]]
+    rotatePattern = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+                     [2, 5, 8, 1, 4, 7, 0, 3, 6],
+                     [0, 1, 2, 3, 4, 5, 6, 7, 8][::-1],
+                     [2, 5, 8, 1, 4, 7, 0, 3, 6][::-1]]
+    flipPattern = [[2, 1, 0, 5, 4, 3, 8, 7, 6],
+                   [0, 3, 6, 1, 4, 7, 2, 5, 8],
+                   [2, 1, 0, 5, 4, 3, 8, 7, 6][::-1],
+                   [0, 3, 6, 1, 4, 7, 2, 5, 8][::-1]]
     allPattern = rotatePattern + flipPattern
 
     for box in posBox:
         if box not in posGoals:
-            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
-                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
-                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1),
+                     (box[0], box[1] - 1), (box[0],
+                                            box[1]), (box[0], box[1] + 1),
+                     (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
             for pattern in allPattern:
                 newBoard = [board[i] for i in pattern]
-                if newBoard[1] in posWalls and newBoard[5] in posWalls: return True
-                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
-                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox: return True
-                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox: return True
-                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
+                if newBoard[1] in posWalls and newBoard[5] in posWalls:
+                    return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls:
+                    return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox:
+                    return True
+                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox:
+                    return True
+                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls:
+                    return True
     return False
 
+
 """Implement all approcahes"""
+
 
 def breadthFirstSearch():
     """Implement breadthFirstSearch approach"""
     beginBox = PosOfBoxes(gameState)
     beginPlayer = PosOfPlayer(gameState)
 
-    startState = (beginPlayer, beginBox) # e.g. ((2, 2), ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5)))
-    frontier = collections.deque([[startState]]) # store states
-    actions = collections.deque([[0]]) # store actions
+    # e.g. ((2, 2), ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5)))
+    startState = (beginPlayer, beginBox)
+    frontier = collections.deque([[startState]])  # store states
+    actions = collections.deque([[0]])  # store actions
     exploredSet = set()
     while frontier:
         node = frontier.popleft()
-        node_action = actions.popleft() 
+        node_action = actions.popleft()
         if isEndState(node[-1][-1]):
-            print(','.join(node_action[1:]).replace(',',''))
-            break
+            print(','.join(node_action[1:]).replace(',', ''))
+            return node_action[1:]
         if node[-1] not in exploredSet:
             exploredSet.add(node[-1])
             for action in legalActions(node[-1][0], node[-1][1]):
-                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                newPosPlayer, newPosBox = updateState(
+                    node[-1][0], node[-1][1], action)
                 if isFailed(newPosBox):
                     continue
                 frontier.append(node + [(newPosPlayer, newPosBox)])
                 actions.append(node_action + [action[-1]])
+
 
 def depthFirstSearch():
     """Implement depthFirstSearch approach"""
@@ -161,21 +197,23 @@ def depthFirstSearch():
     startState = (beginPlayer, beginBox)
     frontier = collections.deque([[startState]])
     exploredSet = set()
-    actions = [[0]] 
+    actions = [[0]]
     while frontier:
         node = frontier.pop()
         node_action = actions.pop()
         if isEndState(node[-1][-1]):
-            print(','.join(node_action[1:]).replace(',',''))
-            break
+            print(','.join(node_action[1:]).replace(',', ''))
+            return node_action[1:]
         if node[-1] not in exploredSet:
             exploredSet.add(node[-1])
             for action in legalActions(node[-1][0], node[-1][1]):
-                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                newPosPlayer, newPosBox = updateState(
+                    node[-1][0], node[-1][1], action)
                 if isFailed(newPosBox):
                     continue
                 frontier.append(node + [(newPosPlayer, newPosBox)])
                 actions.append(node_action + [action[-1]])
+
 
 def heuristic(posPlayer, posBox):
     """A heuristic function to calculate the overall distance between the else boxes and the else goals"""
@@ -184,12 +222,15 @@ def heuristic(posPlayer, posBox):
     sortposBox = list(set(posBox).difference(completes))
     sortposGoals = list(set(posGoals).difference(completes))
     for i in range(len(sortposBox)):
-        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + \
+            (abs(sortposBox[i][1] - sortposGoals[i][1]))
     return distance
+
 
 def cost(actions):
     """A cost function"""
     return len([x for x in actions if x.islower()])
+
 
 def uniformCostSearch():
     """Implement uniformCostSearch approach"""
@@ -206,17 +247,19 @@ def uniformCostSearch():
         node = frontier.pop()
         node_action = actions.pop()
         if isEndState(node[-1][-1]):
-            print(','.join(node_action[1:]).replace(',',''))
-            break
+            print(','.join(node_action[1:]).replace(',', ''))
+            return node_action[1:]
         if node[-1] not in exploredSet:
             exploredSet.add(node[-1])
             Cost = cost(node_action[1:])
             for action in legalActions(node[-1][0], node[-1][1]):
-                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                newPosPlayer, newPosBox = updateState(
+                    node[-1][0], node[-1][1], action)
                 if isFailed(newPosBox):
                     continue
                 frontier.push(node + [(newPosPlayer, newPosBox)], Cost)
                 actions.push(node_action + [action[-1]], Cost)
+
 
 def aStarSearch():
     """Implement aStarSearch approach"""
@@ -233,24 +276,28 @@ def aStarSearch():
         node = frontier.pop()
         node_action = actions.pop()
         if isEndState(node[-1][-1]):
-            print(','.join(node_action[1:]).replace(',',''))
-            break
+            print(','.join(node_action[1:]).replace(',', ''))
+            return node_action[1:]
         if node[-1] not in exploredSet:
             exploredSet.add(node[-1])
             Cost = cost(node_action[1:])
             for action in legalActions(node[-1][0], node[-1][1]):
-                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                newPosPlayer, newPosBox = updateState(
+                    node[-1][0], node[-1][1], action)
                 if isFailed(newPosBox):
                     continue
                 Heuristic = heuristic(newPosPlayer, newPosBox)
-                frontier.push(node + [(newPosPlayer, newPosBox)], Heuristic + Cost) 
+                frontier.push(
+                    node + [(newPosPlayer, newPosBox)], Heuristic + Cost)
                 actions.push(node_action + [action[-1]], Heuristic + Cost)
 
 
 """Read command"""
+
+
 def readCommand(argv):
     from optparse import OptionParser
-    
+
     parser = OptionParser()
     parser.add_option('-l', '--level', dest='sokobanLevels',
                       help='level of game to play', default='level1.txt')
@@ -258,7 +305,7 @@ def readCommand(argv):
                       help='research method', default='bfs')
     args = dict()
     options, _ = parser.parse_args(argv)
-    with open('sokobanLevels/'+options.sokobanLevels,"r") as f: 
+    with open('sokobanLevels/'+options.sokobanLevels, "r") as f:
         layout = f.readlines()
     args['layout'] = layout
     args['method'] = options.agentMethod
@@ -271,15 +318,34 @@ if __name__ == '__main__':
     gameState = transferToGameState(layout)
     posWalls = PosOfWalls(gameState)
     posGoals = PosOfGoals(gameState)
+    solution = []
     if method == 'astar':
-        aStarSearch()
+        solution = aStarSearch()
     elif method == 'dfs':
-        depthFirstSearch()
+        solution = depthFirstSearch()
     elif method == 'bfs':
-        breadthFirstSearch()
+        solution = breadthFirstSearch()
     elif method == 'ucs':
-        uniformCostSearch()
+        solution = uniformCostSearch()
     else:
         raise ValueError('Invalid method.')
-    time_end=time.time()
-    print('Runtime of %s: %.2f second.' %(method, time_end-time_start))
+    board = BoardManager(layout)
+    renderer = Renderer(board).setCaption("Sokoban")
+    renderer.render()
+    is_buttonclick = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                is_buttonclick = True
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        if is_buttonclick == True and len(solution) > 0:
+            act = solution.pop(0)
+            board.movePlayer(act.lower())
+            renderer.fromInstance(board).render()
+            pygame.time.wait(50)
+        elif is_buttonclick == True and len(solution) == 0:
+            renderer.showMessageBox(message='Sokoban solved!')
+
+    time_end = time.time()
+    print('Runtime of %s: %.2f second.' % (method, time_end-time_start))
